5f18d7fa0d460530c22a5c97aa8fda6a5b1d9812
wikiru <1023qaz@naver.com>
2016-11-05 21:09:24 +0900

    add logo

--- acme.skin.php
+++ acme.skin.php
@@ -76,7 +76,7 @@ class AcmeTemplate extends BaseTemplate {
 		<div class="navbar navbar-default navbar-static-top container">
 			<div class="navbar-header">
 				<button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button"><span class="icon-bar"></span> <span class="icon-bar"></span>
-				<span class="icon-bar"></span></button> <a class="navbar-brand" href="<?php echo $this->data['nav_urls']['mainpage']['href']; ?>"><img src='<?php echo $wgLogo; ?>' width='200px'></a>
+				<span class="icon-bar"></span></button> <a class="navbar-brand" href="<?php echo $this->data['nav_urls']['mainpage']['href']; ?>"><img src='https://www.osawiki.com/w/images/c/c9/Logo.png' width='200px'></a>
 			</div>
 
 			<div class="navbar-collapse collapse">
